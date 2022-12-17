f8c87db8d4b6140412e59190cb85dcf23ed28e08
Ozziur <casaruizzo@yahoo.it>
2022-12-17 14:04:25 +0100

    ex00 finito

--- CPP Module 02/ex00/Fixed.cpp
+++ CPP Module 02/ex00/Fixed.cpp
@@ -6,7 +6,45 @@
 /*   By: mruizzo <mruizzo@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 19:43:56 by mruizzo           #+#    #+#             */
-/*   Updated: 2022/12/16 19:43:57 by mruizzo          ###   ########.fr       */
+/*   Updated: 2022/12/17 14:03:07 by mruizzo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_fixed_p_value = 0;
+}
+
+Fixed::Fixed(const Fixed &rhs)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->setRawBits(rhs.getRawBits());
+}
+
+Fixed &Fixed::operator=(const Fixed &rhs)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+	if(this != &rhs)
+		this->_fixed_p_value = rhs.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+	return this->_fixed_p_value;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_fixed_p_value = raw;
+}
--- CPP Module 02/ex00/Fixed.hpp
+++ CPP Module 02/ex00/Fixed.hpp
@@ -6,15 +6,15 @@
 /*   By: mruizzo <mruizzo@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 19:43:40 by mruizzo           #+#    #+#             */
-/*   Updated: 2022/12/16 19:57:53 by mruizzo          ###   ########.fr       */
+/*   Updated: 2022/12/17 13:56:18 by mruizzo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 class Fixed
 {
 private:
-    int _fixed_p_value;
-    static const int    _fra_bit = 8;
+    int                 _fixed_p_value;
+    static const int    _raw_bit = 8;
 public:
     Fixed();
     Fixed(const Fixed &rhs);
