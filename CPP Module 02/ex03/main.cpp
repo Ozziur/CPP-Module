28b1bb064e50f907de0ef529361a068e28a13408
Ozziur <casaruizzo@yahoo.it>
2022-12-18 16:10:27 +0100

    vamos

--- CPP Module 02/ex03/main.cpp
+++ CPP Module 02/ex03/main.cpp
@@ -6,12 +6,12 @@
 /*   By: mruizzo <mruizzo@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 19:27:13 by mruizzo           #+#    #+#             */
-/*   Updated: 2022/12/18 15:44:59 by mruizzo          ###   ########.fr       */
+/*   Updated: 2022/12/18 16:09:24 by mruizzo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*Ciao, sono il Michele del presente(dal tuo punto di vista il Michele del passato),
-e ti sto scrivendo perchè conosco la tua memoria di merda e ti scorderai loa strategia
+e ti sto scrivendo perchè conosco la tua memoria di merda e ti scorderai la strategia
 che stai utilizando per finire questo esercizio. Il punto è semplice: calcola l'area 
 del triangolo ABC. Poi calcoli l'area del triangolo ABP, ACP, CBP. Se la loro somma è uguale a 
 ABC il triangolo è dentro, se no è fuori. Ah, evita di cancellare sto commento: la norma non esiste
