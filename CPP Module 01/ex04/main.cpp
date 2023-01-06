4ccf297120929e6222106e6ce7e645e3d11411eb
MRuizzo <casaruizzo@yahoo.it>
2023-01-06 16:20:00 +0100

    new sed

--- CPP Module 01/ex04/main.cpp
+++ CPP Module 01/ex04/main.cpp
@@ -3,23 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mruizzo <mruizzo@student.42roma.it>        +#+  +:+       +#+        */
+/*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 15:07:03 by mruizzo           #+#    #+#             */
-/*   Updated: 2022/12/15 15:48:08 by mruizzo          ###   ########.fr       */
+/*   Updated: 2023/01/06 16:15:44 by mruizzo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "SedV2.hpp"
+#include <fstream>
+#include <iostream>
 
-int main(int argc, char **argv)
+int	replace(char **argv, std::string str)
 {
-	if(argc != 4)
+	std::ofstream	outfile;
+	int				pos;
+
+	outfile.open((std::string(argv[1]) + ".replace").c_str());
+	if (outfile.fail())
+		return (1);
+	for (int i = 0; i < (int)str.size(); i++)
+	{
+		pos = str.find(argv[2], i);
+		if (pos != -1 && pos == i)
+		{
+			outfile << argv[3];
+			i += std::string(argv[2]).size() - 1;
+		}
+		else
+			outfile << str[i];
+	}
+	outfile.close();
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	char			c;
+	std::ifstream	infile;
+	std::string		str;
+
+	if (argc != 4)
 	{
 		std::cerr<< "Error" << std::endl;
 		return EXIT_FAILURE;
 	}
-	SedV2 sed2(argv[1]);
-	sed2.replace(argv[2], argv[3]);
-	return EXIT_SUCCESS;
+	infile.open(argv[1]);
+	if (infile.fail())
+	{
+		std::cerr << "BOOM!!!!!" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	while(!infile.eof() && infile >> std::noskipws >> c)
+		str += c;
+	infile.close();
+	return (replace(argv, str));
 }
