376b55f6c9f624a990ff72ba32532b22efe0eac2
MRuizzo <casaruizzo@yahoo.it>
2023-01-06 19:09:06 +0100

    .

--- CPP Module 05/ex01/main.cpp
+++ CPP Module 05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 16:10:26 by mruizzo           #+#    #+#             */
-/*   Updated: 2023/01/06 17:35:39 by mruizzo          ###   ########.fr       */
+/*   Updated: 2023/01/06 19:08:22 by mruizzo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int main( void )
 {
 
     try {
-        Bureaucrat bureaucrat("ash",11);
-        Form form("formName", 10);
+        Bureaucrat bureaucrat("Zero Assoluto",10);
+        Form form("Per Dimenticare", 1);
 
         bureaucrat.signForm(form);
 
