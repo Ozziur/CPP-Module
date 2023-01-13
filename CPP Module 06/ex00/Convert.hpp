1d5eb2658d5c776d9ba492b594e23a138ca387f4
MRuizzo <casaruizzo@yahoo.it>
2023-01-13 15:19:38 +0100

    .

--- CPP Module 06/ex00/Convert.hpp
+++ CPP Module 06/ex00/Convert.hpp
@@ -6,7 +6,7 @@
 /*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 12:01:36 by mruizzo           #+#    #+#             */
-/*   Updated: 2023/01/13 12:14:41 by mruizzo          ###   ########.fr       */
+/*   Updated: 2023/01/13 15:16:37 by mruizzo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,6 @@ class Convert {
 		Convert(const Convert& other);
 		Convert& operator=(const Convert& other);
 
-		//static bool tryChar(const std::string& literal);
-		//static bool tryInt(const std::string& literal);
-		//static bool tryFloat(const std::string& literal);
-		//static bool tryDouble(const std::string& literal);
 };
 
 #endif
